a6b64d117cd72feee995af844c079411f611dfdc
Sabin2 <sabinbhurtel@gmail.com>
2023-01-02 09:07:59 +0545

    Creating home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to my blog"
+  // const title = "Welcome to my blog"
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title }</h1>
+        {/* <h1>{title }</h1> */}
+        <Home />
       </div>
     </div>
   );
