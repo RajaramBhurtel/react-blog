2452daa08112870afb10e91e83efdb52cce98042
Sabin2 <sabinbhurtel@gmail.com>
2023-01-02 11:12:24 +0545

    Events in React

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 const Home = () => {
+    const handleClick = (e) =>{
+        console.log('clicked', e);
+    }
+    const handleClickAgain = (name) =>{
+        console.log('clicked by ' + name);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            {/* button click with anyonomous function */}
+            <button onClick={() =>{
+                handleClickAgain("Sabin");
+            }}>Click me again</button>
         </div>
      );
 }
  
 export default Home;
+
+
