ce6841a055f411afefdfa9d318a67bb4447746a8
Sabin2 <sabinbhurtel@gmail.com>
2023-01-03 13:34:33 +0545

    Outputing List

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
-    const handleClick = (e) =>{
-        console.log('clicked', e);
-    }   
+    const [blogs, setBlogs] = useState([
+        {title: 'My new blog', body: 'lorem ipsum...', author: 'Sabin', id: 1},
+        {title: 'Cooking blog', body: 'lorem ipsum...', author: 'Raja', id: 2},
+        {title: 'Sports blog', body: 'lorem ipsum...', author: 'Sanam', id: 3},
+        {title: 'Politics blog', body: 'lorem ipsum...', author: 'Rajendra', id: 4}
+    ]); 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
