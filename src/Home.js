4b747abf509b4723eac1311bfcd98a115da519dc
Sabin2 <sabinbhurtel@gmail.com>
2023-01-03 12:05:31 +0545

    Style and Button removed

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,11 @@
 const Home = () => {
     const handleClick = (e) =>{
         console.log('clicked', e);
-    }
-    const handleClickAgain = (name) =>{
-        console.log('clicked by ' + name);
-    }
+    }   
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            {/* button click with anyonomous function */}
-            <button onClick={() =>{
-                handleClickAgain("Sabin");
-            }}>Click me again</button>
         </div>
      );
 }
