87740f60905336c91e915b4f107da851fd6014fe
Sabin2 <sabinbhurtel@gmail.com>
2023-01-03 16:53:54 +0545

    useEffect and dynamic data from json

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new blog', body: 'lorem ipsum...', author: 'Sabin', id: 1},
-        {title: 'Cooking blog', body: 'lorem ipsum...', author: 'Raja', id: 2},
-        {title: 'Sports blog', body: 'lorem ipsum...', author: 'Sanam', id: 3},
-        {title: 'Politics blog', body: 'lorem ipsum...', author: 'Sabin', id: 4}
-    ]); 
+    const [blogs, setBlogs] = useState(null); 
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs (newBlogs);
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs (newBlogs);
+    // }
+
+    useEffect( () => {
+       fetch("http://localhost:8000/blogs")
+       .then( response => {
+        return response.json();
+       })
+       .then( (data) => {
+        // console.log(data);
+        setBlogs(data)
+       });
+    }, []);
+
     return (
       <div className="home">
-        <BlogList blogs={blogs} title="Blog List" handleDelete={handleDelete}/>
+        {blogs && <BlogList blogs={blogs} title="Blog List" />}
       </div>
     );
 }
