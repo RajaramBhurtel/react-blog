1ece67086d7a23526e9e254024268d8a2543c145
Sabin2 <sabinbhurtel@gmail.com>
2023-01-04 11:18:43 +0545

    Defining basic react routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   // const title = "Welcome to my blog"
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        {/* <h1>{title }</h1> */}
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* <h1>{title }</h1> */}
+          {/* <Home /> */}
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
