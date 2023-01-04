f781e935b12a01353f275879cc30b8b0c2ab7625
Sabin2 <sabinbhurtel@gmail.com>
2023-01-04 12:41:23 +0545

    Creating add blog form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add a new blog</h2>
-        </div>
-     );
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState("Sabin");
+    
+
+    return (
+      <div className="create">
+        <h2>Add a new blog</h2>
+        <form action="">
+          <label> Blog Title:</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <label> Blog Body:</label>
+          <textarea
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            required
+          ></textarea>
+          <label>Blog Author:</label>
+          <select 
+          value={author} 
+          onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value="Sabin">Sabin</option>
+            <option value="Raja">Raja</option>
+            <option value="Ram">Ram</option>
+          </select>
+          <button>Add Blog</button>
+          <p>
+            {title} {body} {author}
+          </p>
+        </form>
+      </div>
+    );
 }
  
 export default Create;
