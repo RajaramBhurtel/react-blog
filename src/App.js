0f723b1167b564f4b3277fa68dfe5bb385ad2d6f
Sabin2 <sabinbhurtel@gmail.com>
2023-01-05 09:32:41 +0545

    Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import New from "./app/store";
 
 function App() {
   // const title = "Welcome to my blog"
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        {/* <h1>{title }</h1> */}
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* <h1>{title }</h1> */}
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<New />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
